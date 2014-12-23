91679c8ed1b2fdbf10f5296905e05f74b8f10667
Carton He <carton.he@marvell.com>
2014-12-23 11:17:21 +0800

    Update reame.txt with more info
    And also use new implementation for compare_suminfo.sh
    
    Change-Id: I6fb9d16f83fb8838fe9f60253e43fd75fb23c0fd
    Signed-off-by: Carton He <carton.he@marvell.com>

--- scripts/compare_suminfo.sh
+++ scripts/compare_suminfo.sh
@@ -1,69 +1,39 @@
 #!/bin/bash
+if [ $# -ne 2 ]
+then
+    echo "usage: $0 sum1.log sum2.log"
+    exit
+fi
 
-
-get_value()
-{
-	perl -nale "if (/^$1/) {print \$F[1];}" $2
-}
-
-get_gc_value()
-{
-	perl -nale "if (/$1/) {print \$F[2];}" $2
-}
-
-# file 2
-file=$1
-
-buffers1=$(get_value "Buffers:" "$file")
-cached1=$(get_value "Cached:" "$file")
-
-anonpages1=$(get_value "AnonPages:" "$file")
-
-memtotal1=$(get_value "MemTotal:" "$file")
-memfree1=$(get_value "MemFree:" "$file")
-
-freesum1=$(($memfree1+$cached1+$buffers1))
-
-contig1=$(get_gc_value "contiguousPaged:" "$file")
-contignon1=$(get_gc_value "contiguousNonPaged:" "$file")
-gc_virtual1=$(get_gc_value "virtualPaged:" "$file")
-
-gc_alloc1=$(($contig1+$contignon1+$gc_virtual1))
-
-gc_total1=$(perl -nale "if (/GC Memory Sum:/) {print \$F[3];}" "$file")
-
-# file 2
-file=$2
-
-buffers2=$(get_value "Buffers:" "$file")
-cached2=$(get_value "Cached:" "$file")
-
-anonpages2=$(get_value "AnonPages:" "$file")
-
-memtotal2=$(get_value "MemTotal:" "$file")
-memfree2=$(get_value "MemFree:" "$file")
-
-freesum2=$(($memfree2+$cached2+$buffers2))
-
-contig2=$(get_gc_value "contiguousPaged:" "$file")
-contignon2=$(get_gc_value "contiguousNonPaged:" "$file")
-gc_virtual2=$(get_gc_value "virtualPaged:" "$file")
-
-gc_alloc2=$(($contig2+$contignon2+$gc_virtual2))
-
-gc_total2=$(perl -nale "if (/GC Memory Sum:/) {print \$F[3];}" "$file")
-
-mem_incr_diff=$(($freesum1-$freesum2))
-anonpages_diff=$(($anonpages2-$anonpages1))
-gc_alloc_diff=$(($gc_alloc2-$gc_alloc1))
-gc_total_diff=$(($gc_total2-$gc_total1))
-other_diff=$(($mem_incr_diff - $gc_alloc_diff - $anonpages_diff ))
-
-echo "Mem Incr diff:" $mem_incr_diff
-echo "Anonpages diff:" $anonpages_diff
-echo "GC Total diff:" $gc_total_diff
-echo "GC Alloc diff:" $gc_alloc_diff
-echo "Other diff:" $other_diff
-
-echo -e "$mem_incr_diff\t$anonpages_diff\t$gc_total_diff\t$gc_alloc_diff"
-
+res_file="table.txt"
+rm -f $res_file
+num=0
+while read line1
+do
+    if [ $num -eq 0 ]
+    then
+        log_name2=`head -1 $2`
+        echo "| $line1 | $log_name2" >> $res_file
+        num=1
+        continue
+    fi
+    # get the memory size
+    i=`expr index "$line1" ":"`
+    if [ "$i" == "0" ]
+    then
+        echo "$line1: i: $i"
+        continue
+    fi
+    size=`echo $line1 | cut -d ":" -f 2`
+    # get the item name
+    item=`echo $line1 | cut -d ":" -f 1`
+    echo "size: $size; item: $item"
+    if [ "$size" != "" ] && [ "$item" != "" ]
+    then
+        size2=`grep "$item:" $2 | cut -d ":" -f 2`
+        echo "$item | $size | $size2" >> $res_file
+    elif [ "$size" == "" ] && [ "$item" != "" ]
+    then
+        echo "$item" >> $res_file
+    fi
+done < $1
