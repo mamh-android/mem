1c0bc95be5247ece4ac2833b06163707af9686cd
Carton He <carton.he@marvell.com>
2014-12-23 11:26:28 +0800

    Change to use TAB as delimeter and other minor fixes
    Change-Id: I5ff7c9ad328a770a7ec5eebb84daf29a40579eb5
    Signed-off-by: Carton He <carton.he@marvell.com>

--- scripts/compare_suminfo.sh
+++ scripts/compare_suminfo.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+# Parse two "suminfo.log" and generate table to be imported to Excel
+
+DELIM="\t"
+res_file="mem_summary_compare.txt"
+
 if [ $# -ne 2 ]
 then
     echo "usage: $0 sum1.log sum2.log"
     exit
 fi
 
-res_file="table.txt"
-rm -f $res_file
 num=0
 while read line1
 do
     if [ $num -eq 0 ]
     then
         log_name2=`head -1 $2`
-        echo "| $line1 | $log_name2" >> $res_file
+        echo -e "${DELIM}$line1${DELIM}$log_name2" > $res_file
         num=1
         continue
     fi
@@ -31,7 +34,7 @@ do
     if [ "$size" != "" ] && [ "$item" != "" ]
     then
         size2=`grep "$item:" $2 | cut -d ":" -f 2`
-        echo "$item | $size | $size2" >> $res_file
+        echo -e "$item${DELIM}$size${DELIM}$size2" >> $res_file
     elif [ "$size" == "" ] && [ "$item" != "" ]
     then
         echo "$item" >> $res_file
