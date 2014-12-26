d403b22790b96a3a8ce384e38da68af528ee82de
Carton He <carton.he@marvell.com>
2014-12-26 10:51:31 +0800

    Fix no alloc trace issue
    Change-Id: I4dfaa5b9aed552ba3782ffd79229405d40a91342
    Signed-off-by: Carton He <carton.he@marvell.com>

--- scripts/ftrace_enable_memtrace.sh
+++ scripts/ftrace_enable_memtrace.sh
@@ -7,8 +7,9 @@ adb_root
 
 $ADB_SHELL 'echo 0 > /d/tracing/tracing_on'
 
-$ADB_SHELL 'echo "order > 0" > /d/tracing/events/kmem/mm_page_alloc/filter'
-$ADB_SHELL 'echo "order >= 0" > /d/tracing/events/kmem/mm_page_free/filter'
+# You may change here to filter alloc/free with specified order
+# $ADB_SHELL 'echo "order >= 0" > /d/tracing/events/kmem/mm_page_alloc/filter'
+# $ADB_SHELL 'echo "order >= 0" > /d/tracing/events/kmem/mm_page_free/filter'
 $ADB_SHELL 'echo "kmem:mm_page_alloc" > /d/tracing/set_event'
 # Notice the '>>' used here
 $ADB_SHELL 'echo "kmem:mm_page_free" >> /d/tracing/set_event'
